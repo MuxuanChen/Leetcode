4e816d14444e40949ce537be1ebf93a3085b39a9
MuxuanChan <muxuanchan@gmail.com>
2020-03-30 22:59:55 +0800

    add 数学 + 循环解法

--- src/main/java/math/LastRemaining62.java
+++ src/main/java/math/LastRemaining62.java
@@ -28,6 +28,25 @@ public class LastRemaining62 {
         return (x + m) % n;
     }
 
+    /**
+     * 方法二：数学 + 循环
+     * <p>
+     * 最终剩下一个人时的安全位置肯定为0，反推安全位置在人数为n时的编号，每次都是移动m个位置
+     * 人数为1： 0
+     * 人数为2： (0+m) % 2
+     * 人数为3： ((0+m) % 2 + m) % 3
+     * 人数为4： (((0+m) % 2 + m) % 3 + m) % 4
+     * ........
+     * 迭代推理到n就可以得出答案
+     */
+    public int lastRemaining2(int n, int m) {
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            res = (res + m) % i;
+        }
+        return res;
+    }
+
 
     /**
      * 在数据量大的时候会超时，通不过LeetCode的检查
