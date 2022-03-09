09a7e8e9e3581905246f7c603952a149b82e7c39
chenhaixiang <haixiang7c6@gmail.com>
2022-03-09 10:54:52 +0800

    四数相加II map containKey判断是否存在 map.put(key,1) map.put(key, map.get(key) + 1)

---
+++ src/main/java/map/FourSumCount454.java
@@ -0,0 +1,48 @@
+package map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 四数相加 II
+ * 给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l) 能满足：
+ * <p>
+ * 0 <= i, j, k, l < n
+ * nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/4sum-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Haixiang
+ * @since 2022-03-09
+ */
+public class FourSumCount454 {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int temp;
+        for (int i : nums1) {
+            for (int j : nums2) {
+                temp = i + j;
+                if (map.containsKey(temp)) {
+                    map.put(temp, map.get(temp) + 1);
+                } else {
+                    map.put(temp, 1);
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i : nums3) {
+            for (int j : nums4) {
+                temp = -(i + j);
+                if (map.containsKey(temp)) {
+                    count += map.get(temp);
+                }
+            }
+        }
+
+        return count;
+    }
+}
