bdc2a39438eff0d4e22e89df74308d92d8787f5f
haixiang <haixiang7c6@gmail.com>
2022-06-28 09:40:58 +0800

    最长子序列

---
+++ src/main/java/dp/childSeq/LengthOfLIS300.java
@@ -0,0 +1,37 @@
+package dp.childSeq;
+
+import java.util.Arrays;
+
+/**
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ * <p>
+ * 子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/longest-increasing-subsequence
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author haixiang
+ * @since 2022-06-28
+ */
+public class LengthOfLIS300 {
+    public int lengthOfLIS(int[] nums) {
+        int length = nums.length;
+        if (length <= 1) {
+            return length;
+        }
+        //默认子序列长度至少为1
+        int result = 0;
+        int[] dp = new int[length];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+        return result;
+    }
+}
