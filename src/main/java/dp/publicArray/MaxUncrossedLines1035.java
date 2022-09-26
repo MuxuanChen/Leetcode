d22bccef6dfc83530047595ac70c2eb84634a29b
haixiang <chenhx@pingpongx.com>
2022-09-26 19:58:45 +0800

    1035. 不相交的线

---
+++ src/main/java/dp/publicArray/MaxUncrossedLines1035.java
@@ -0,0 +1,35 @@
+package dp.publicArray;
+
+/**
+ * 在两条独立的水平线上按给定的顺序写下 nums1 和 nums2 中的整数。
+ * <p>
+ * 现在，可以绘制一些连接两个数字 nums1[i] 和 nums2[j] 的直线，这些直线需要同时满足满足：
+ * <p>
+ *  nums1[i] == nums2[j]
+ * 且绘制的直线不与任何其他连线（非水平线）相交。
+ * 请注意，连线即使在端点也不能相交：每个数字只能属于一条连线。
+ * <p>
+ * 以这种方法绘制线条，并返回可以绘制的最大连线数。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/uncrossed-lines
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Haixiang
+ * @since 2022-09-26
+ */
+public class MaxUncrossedLines1035 {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        for (int i = 1; i <= nums1.length; i++) {
+            for (int j = 1; j <= nums2.length; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[nums1.length][nums2.length];
+    }
+}
